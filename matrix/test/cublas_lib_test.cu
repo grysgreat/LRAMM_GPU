fb8d9995e7b1f9a8368283a38a742da744c465b6
StarGrys <771582678@qq.com>
2024-10-10 19:29:35 +0800

    cublas gemv performance test.

--- matrix/test/cublas_lib_test.cu
+++ matrix/test/cublas_lib_test.cu
@@ -293,6 +293,197 @@ void gemv_acc_test(){
 }
 
 
+
+void performance_test(){
+    int test_para[2048][5] = {
+
+        // {2048,2048,2048,50},
+        // {2048,2048,2048,40},
+        // {2048,2048,2048,30},
+        // {2048,2048,2048,20},
+        // {2048,2048,2048,10},
+        // {2048,2048,2048,1},
+        // {16384,16384,16384,1},
+    {128,128},
+    {256,256},
+    {384,384},
+    {512,512},
+    {640,640},
+    {768,768},
+    {896,896},
+    {1024,1024},
+    {1152,1152},
+    {1280,1280},
+    {1408,1408},
+    {1536,1536},
+    {1664,1664},
+    {1792,1792},
+    {1920,1920},
+    {2048,2048},
+    {2176,2176},
+    {2304,2304},
+    {2432,2432},
+    {2560,2560},
+    {2688,2688},
+    {2816,2816},
+    {2944,2944},
+    {3072,3072},
+    {3200,3200},
+    {3328,3328},
+    {3456,3456},
+    {3584,3584},
+    {3712,3712},
+    {3840,3840},
+    {3968,3968},
+    {4096,4096},
+    {4224,4224},
+    {4352,4352},
+    {4480,4480},
+    {4608,4608},
+    {4736,4736},
+    {4864,4864},
+    {4992,4992},
+    {5120,5120},
+    {5248,5248},
+    {5376,5376},
+    {5504,5504},
+    {5632,5632},
+    {5760,5760},
+    {5888,5888},
+    {6016,6016},
+    {6144,6144},
+    {6272,6272},
+    {6400,6400},
+    {6528,6528},
+    {6656,6656},
+    {6784,6784},
+    {6912,6912},
+    {7040,7040},
+    {7168,7168},
+    {7296,7296},
+    {7424,7424},
+    {7552,7552},
+    {7680,7680},
+    {7808,7808},
+    {7936,7936},
+    {8064,8064},
+    {8192,8192},
+    {8320,8320},
+    {8448,8448},
+    {8576,8576},
+    {8704,8704},
+    {8832,8832},
+    {8960,8960},
+    {9088,9088},
+    {9216,9216},
+    {9344,9344},
+    {9472,9472},
+    {9600,9600},
+    {9728,9728},
+    {9856,9856},
+    {9984,9984},
+    {10112,10112},
+    {10240,10240},
+    {10368,10368},
+    {10496,10496},
+    {10624,10624},
+    {10752,10752},
+    {10880,10880},
+    {11008,11008},
+    {11136,11136},
+    {11264,11264},
+    {11392,11392},
+    {11520,11520},
+    {11648,11648},
+    {11776,11776},
+    {11904,11904},
+    {12032,12032},
+    {12160,12160},
+    {12288,12288},
+    {12416,12416},
+    {12544,12544},
+    {12672,12672},
+    {12800,12800},
+    {12928,12928},
+    {13056,13056},
+    {13184,13184},
+    {13312,13312},
+    {13440,13440},
+    {13568,13568},
+    {13696,13696},
+    {13824,13824},
+    {13952,13952},
+    {14080,14080},
+    {14208,14208},
+    {14336,14336},
+    {14464,14464},
+    {14592,14592},
+    {14720,14720},
+    {14848,14848},
+    {14976,14976},
+    {15104,15104},
+    {15232,15232},
+    {15360,15360},
+    {15488,15488},
+    {15616,15616},
+    {15744,15744},
+    {15872,15872},
+    {16000,16000},
+    {16128,16128},
+    {16256,16256},
+        }; 
+  
+    cublasHandle_t cublasH = NULL;
+    CUBLAS_CHECK(cublasCreate(&cublasH));
+
+    int max = 4096*4;
+    float *matrixA = (float *)malloc(sizeof(float) * max*max);
+    float *matrixB = (float *)malloc(sizeof(float) * max*max);
+    float *matrixC = (float *)malloc(sizeof(float) * max*max);
+
+    char * work;
+    cudaMalloc((char **)&work, sizeof(float) * (max*max*6+max*5));
+
+    std::cout<<"M\tN\tSGEMV\n";
+    const int digit = 8;
+    
+    float *A_d, *B_d, *C_d;
+    generate_matrix<float>(matrixA,max,max,'u');
+    generate_matrix<float>(matrixB,max,max,'u');   
+    cudaMalloc((float **)&A_d, sizeof(float) * max*max);
+    cudaMalloc((float **)&B_d, sizeof(float) * max);
+    cudaMalloc((float **)&C_d, sizeof(float) * max);
+    cudaMemcpy(A_d, matrixA, sizeof(float) * max*max, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, matrixB, sizeof(float) * max, cudaMemcpyHostToDevice);    
+    for(int i=0;i<128;i++){
+
+        int N=test_para[i][0],M=test_para[i][1];
+        
+        float alpha = 1.0, beta = 0.0;
+        if(M==0) return;
+        cudaDeviceSynchronize();
+        std::cout<<M<<"\t"<<N<<"\t";
+
+        //计算float和int矩阵乘法得到结果矩阵
+
+        // xgemm(matrixA,matrixB,matrixC,M,K,K,N);
+          beta = 0.0, alpha = 1.0;
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+            cublas_gemv_rowmajor( &cublasH,A_d, B_d, C_d, M, N, alpha, beta);
+            cudaDeviceSynchronize();
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> diff = end - start;
+            double time  = diff.count();
+            printf("%.7lf\n",time);
+            cudaDeviceSynchronize();
+        }
+
+    }
+    return;        
+}
+
+
 void hgemm_perf_test(){
     cublasHandle_t cublasH = NULL;
     CUBLAS_CHECK(cublasCreate(&cublasH));
@@ -433,6 +624,7 @@ int main(){
     //gemv_acc_test();
     //gemm_acc_test();
     //gemm_acc_test2();
-    hgemm_acc_test();
+    //hgemm_acc_test();
+    performance_test();
     return 0;
 }
